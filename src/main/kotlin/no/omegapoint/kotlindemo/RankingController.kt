1ce5f570c1986b88b9fec3864dc1049bfc9b45e4
Yong Bin Kwon <yongbinkwon97@gmail.com>
2023-09-12 22:02:58 +0200

    add skeleton RankingController

---
+++ src/main/kotlin/no/omegapoint/kotlindemo/RankingController.kt
@@ -0,0 +1,11 @@
+package no.omegapoint.kotlindemo
+
+//create methods theBest() and theWorst() that returns the Strings "bin" and "tom henrik" respectively
+class RankingController {
+
+}
+
+fun main() {
+    println(theBest())
+    println(theWorst())
+}
