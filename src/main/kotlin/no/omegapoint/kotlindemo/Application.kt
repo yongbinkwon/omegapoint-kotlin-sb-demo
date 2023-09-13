6617a03775198c8252af009d11844e0af8e2eb5c
Yong Bin Kwon <yongbinkwon97@gmail.com>
2023-09-13 09:19:11 +0200

    disable db autoconfiguration

--- src/main/kotlin/no/omegapoint/kotlindemo/Application.kt
+++ src/main/kotlin/no/omegapoint/kotlindemo/Application.kt
@@ -1,10 +1,11 @@
 package no.omegapoint.kotlindemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
 @EnableScheduling
 class Application
 
